fc3913cd6a3bbf32c28ef32d435f4c884ce37c52
alesyfrontend <alesy2007@gmail.com>
2024-08-19 08:47:19 -0300

    decodificador

--- script.js
+++ script.js
@@ -1,21 +1,117 @@
 
-
-
-
-
 const textArea=document.querySelector(".input-text");
 const mensagem =document.querySelector(".mensagem");
 
+function validarTextoECriptografar(){
+	let texto = textArea.value.trim();
+	let regex = /[A-ZÀ-Ýà-ÿ]/u;
+	let alerta = document.getElementById("mensagemAlerta");
+	
 
+	if (regex.test(texto)){
+		alerta.textContent = "👎🏽 Texto inválido!";
+		
+	} else if (texto === ""){
+		
+		alerta.textContent = "💁 Nenhum texto digitado."
+	} else {
+		
+		alerta.textContent = "🔒 Mensagem criptografada!";
+	
+		botaoCriptografar();
+	}
+}
+
+
+function validarTextoEDescriptografar(){
+	let texto = textArea.value.trim();
+	let regex = /[A-ZÀ-Ýà-ÿ]/u;
+	let alerta = document.getElementById("mensagemAlerta");
+
+
+
+	if (regex.test(texto)){
+		alerta.textContent = "👎🏽 Texto inválido!";
+	
+	} else if (texto === ""){
+	
+		alerta.textContent = "💁 Nenhum texto digitado."
+	} else {
+
+		alerta.textContent = "🔓 Mensagem Descriptografada!";
+		botaoDescriptografar();
+	}
+}
+
+
+function botaoCriptografar(){
+	const textoEncriptado = criptografar(textArea.value);
+	mensagem.value = textoEncriptado;
+	textArea.value = "";
+}
+
+function botaoDescriptografar(){
+	const textoDesencriptado = descriptografar(textArea.value);
+	mensagem.value = textoDesencriptado;
+	textArea.value = "";
+}
+
+
+function criptografar (stringEncriptada){
+	let criptografia = [["e", 'enter'] , ["i", "imes"], ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+
+	stringEncriptada = stringEncriptada;
+
+	for (let i = 0; i < criptografia.length; i++){
+		if (stringEncriptada.includes(criptografia[i][0])){
+			stringEncriptada = stringEncriptada.replaceAll(criptografia[i][0], criptografia[i][1]);
+		}
+	}
+
+	return stringEncriptada;
+}
+
+
+function descriptografar (stringDesencriptada){
+	let criptografia = [["e", 'enter'] , ["i", "imes"], ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+
+	stringDesencriptada = stringDesencriptada;
+
+	for (let i = 0; i < criptografia.length; i++){
+		if (stringDesencriptada.includes(criptografia[i][1])){
+			stringDesencriptada = stringDesencriptada.replaceAll(criptografia[i][1], criptografia[i][0]);
+		}
+	}
+
+	return stringDesencriptada;
+}
+
+/*
 function btnEncriptar(){
     const textoEncriptado= encriptar(textArea.value);
     mensagem.value=textoEncriptado;
     textArea.value="";
+	let texto = textArea.value.trim();
+	let regex = /[A-ZÀ-Ýà-ÿ]/u;
+	let alerta = document.getElementById("mensagemAlerta");
+	let imagem = document.getElementById("gatinho");
+
+	if (regex.test(texto)){
+		alerta.textContent = "Texto inválido!";
+		imagem.src = "imagens/alerta.png";
+	} else if (texto === ""){
+		imagem.src = "imagens/alerta.png"
+		alerta.textContent = "Nenhum texto digitado."
+	} else {
+		imagem.src = "imagens/chat (1).png";
+		alerta.textContent = "Mensagem criptografada!";
+		btnEncriptar();
+	}
 
 }
 
 
-
+/*
 function encriptar(stringEncriptada){
 
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "obter"], ["u", "ufat"] ]
@@ -34,7 +130,7 @@ function btnDesencriptar(){
   const textoDesencriptado= desencriptar(textArea.value);
   mensagem.value=textoDesencriptado;
   textArea.value="";
-
+ 
 }
 
 
@@ -51,6 +147,7 @@ for (let i= 0; i < matrizCodigo.length; i++){
 }
 return stringDesencriptada;
 }
+*/
 
 function copiarTexto() {
   let alerta = document.getElementById("mensagemAlerta")
@@ -65,6 +162,9 @@ function copiarTexto() {
 
 	limparTexto()
 }
+
+
+
 
 /* Regras Codificador: 
 "e" é convertido para "enter" 
