176495c96de4467fe8eb5acd231c61ecff4ee0d1
alesyfrontend <alesy2007@gmail.com>
2024-08-21 13:58:51 -0300

    txtcriptografado

--- script.js
+++ script.js
@@ -86,68 +86,6 @@ function descriptografar (stringDesencriptada){
 	return stringDesencriptada;
 }
 
-/*
-function btnEncriptar(){
-    const textoEncriptado= encriptar(textArea.value);
-    mensagem.value=textoEncriptado;
-    textArea.value="";
-	let texto = textArea.value.trim();
-	let regex = /[A-ZÀ-Ýà-ÿ]/u;
-	let alerta = document.getElementById("mensagemAlerta");
-	let imagem = document.getElementById("gatinho");
-
-	if (regex.test(texto)){
-		alerta.textContent = "Texto inválido!";
-		imagem.src = "imagens/alerta.png";
-	} else if (texto === ""){
-		imagem.src = "imagens/alerta.png"
-		alerta.textContent = "Nenhum texto digitado."
-	} else {
-		imagem.src = "imagens/chat (1).png";
-		alerta.textContent = "Mensagem criptografada!";
-		btnEncriptar();
-	}
-
-}
-
-
-/*
-function encriptar(stringEncriptada){
-
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "obter"], ["u", "ufat"] ]
-    stringEncriptada = stringEncriptada.toLowerCase();
-
-for (let i= 0; i < matrizCodigo.length; i++){
-     if (stringEncriptada.includes(matrizCodigo[i][0])){
-       stringEncriptada=stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1]);
-     }
-}
- return stringEncriptada;
-
-
-}
-function btnDesencriptar(){
-  const textoDesencriptado= desencriptar(textArea.value);
-  mensagem.value=textoDesencriptado;
-  textArea.value="";
- 
-}
-
-
-
-function desencriptar(stringDesencriptada){
-
-  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "obter"], ["u", "ufat"] ]
-  stringDesencriptada = stringDesencriptada.toLowerCase();
-
-for (let i= 0; i < matrizCodigo.length; i++){
-   if (stringDesencriptada.includes(matrizCodigo[i][1])){
-     stringDesencriptada=stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo [i][0]);
-   }
-}
-return stringDesencriptada;
-}
-*/
 
 function copiarTexto() {
   let alerta = document.getElementById("mensagemAlerta")
